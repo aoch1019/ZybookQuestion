c392c328253985e416789713510f0f40dec1fd2e
andrewoch <andrewoch@10.0.0.193>
2020-10-16 13:04:16 -0400

    Added all required instance variables

--- Recitation Git/src/ItemToPurchase.java
+++ Recitation Git/src/ItemToPurchase.java
@@ -1,6 +1,12 @@
 
 public class ItemToPurchase {
 
+	
+	String itemName;
+	int itemPrice;
+	int itemQuantity;
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
